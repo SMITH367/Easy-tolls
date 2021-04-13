d3cb6c22efe51d3a79fa3afb98418ac589071711
Brayan Puentes <brayansmithpuente@gmail.com>
2021-04-13 18:01:56 -0500

    minichange

--- easyToolsmin.js
+++ easyToolsmin.js
@@ -125,7 +125,7 @@ let currentNode=this.head;while(currentNode){if(currentNode.value===value){retur
 currentNode=currentNode.next}}
 deleteHead(){if(this.head){if(this.head.next){const secondNode=this.head.next;this.head=secondNode}else{this.head=null}}}
 viewElements(){let currentNode=this.head;while(currentNode){console.log(currentNode.value);currentNode=currentNode.next}}}
-const quicksort=(array)=>{if(array.length<=1){return array}
+const quickSort=(array)=>{if(array.length<=1){return array}
 var index=array[0];var left=[];var right=[];for(var i=1;i<array.length;i++){if(array[i]<index){left.push(array[i])}else{right.push(array[i])}}
 return quicksort(left).concat(index,quicksort(right))};const bubbleSort=(array)=>{let sortVar;for(let i=0;i<array.length;i++){for(let j=0;j<array.length-i;j++){if(array[j]>array[j+1]){sortVar=array[j];array[j]=array[j+1];array[j+1]=sortVar}}}
 return array}
@@ -151,7 +151,7 @@ export {
     generateString,
     binarySearch,
     bubbleSort,
-    quicksort,
+    quickSort,
     typeofMethod,
     functionTest
 
